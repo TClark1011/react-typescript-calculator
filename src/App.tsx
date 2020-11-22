3d36e801ac37a694851a504e5dc74f49a6a776ab
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-11-23 09:36:57 +1100

    Set up basic folder architecture and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Calculator from './components/Calculator';
+
 function App() {
 	return (
 		<div className='App'>
 			<div id='calculator'>
-				<h1>Calculator</h1>
+				<Calculator />
 			</div>
 		</div>
 	);
---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,13 @@
+import './Calculator.scss';
+
+import React from 'react';
+
+const Calculator: React.FC = (props) => {
+	return (
+		<div id='calculator'>
+			<h1>A Calculator</h1>
+		</div>
+	);
+};
+
+export default Calculator;
